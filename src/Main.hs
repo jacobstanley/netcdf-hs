810c066cbea19c4fa5a717044a8265c53cfb560c
Jacob Stanley <jacob@stanley.io>
2012-01-30 12:32:50 +0800

    A few different experiments in Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,63 @@
-module Main (main) where
+module Main where
 
-import Control.Monad.IO.Class (liftIO)
+import Data.Attoparsec.Binary
+--import Data.Attoparsec.IEEE754
+import Data.ByteString.Char8 (unpack)
 import Data.Conduit (($$), runResourceT)
 import Data.Conduit.Attoparsec (sinkParser)
-import Data.Conduit.Binary (sourceFile)
-import Prelude hiding (take, drop)
-import Text.Show.Pretty (ppShow)
+import Data.Conduit.Binary (sourceFile, sourceFileRange)
+import Data.List
+import Data.Ord
+import Prelude
 
 import Data.NetCDF.Parser (header)
+import Data.NetCDF.Types
 
 ------------------------------------------------------------------------
 
 main :: IO ()
-main = runResourceT $ do
-    nc <- file $$ parser
-    liftIO (putStrLn $ ppShow nc)
+main = do
+    hd <- parseHeader file
+
+    let vs  = hdrVars hd
+        v   = ((!! 1) . filter ((== VarShort) . varType)) vs
+
+        vs' = sortBy (comparing varOffset)
+            $ filter isRecordVar vs
+        v'  = varOffset (head vs')
+        ws  = map (\x -> unpack (varName x) ++ ": " ++ show (varOffset x - v')) vs'
+
+    print (hdrNumRecs hd)
+    putStrLn (unlines ws)
+
+    mapM_ (putStrLn . showVar) [v]
+
+    print (varSize v)
+    print (varOffset v)
+    x <- readNumber file (varOffset v + 4 * 100)
+    print x
   where
-    file   = sourceFile "/home/jake/data/netcdf/sresa1b_ncar_ccsm3_0_run1_200001.nc"
+    file = "/home/jake/data/netcdf/cami_0000-09-01_64x128_L26_c030918.nc"
+
+showVar :: Var -> String
+showVar v = unpack (varName v) ++ "\n" ++ unlines (map showDim (varDims v))
+
+showDim :: Dim -> String
+showDim d = "  " ++ unpack (dimName d) ++ " (" ++ show (dimLength d) ++ ")"
+
+isRecordVar :: Var -> Bool
+isRecordVar = any (== Unlimited) . map dimLength . varDims
+
+------------------------------------------------------------------------
+
+parseHeader :: FilePath -> IO Header
+parseHeader path = runResourceT (file $$ parser)
+  where
+    file   = sourceFile path
     parser = sinkParser header
+
+readNumber :: FilePath -> FileOffset -> IO Integer
+readNumber path offset = runResourceT (file $$ parser)
+  where
+    file   = sourceFileRange path (Just $ fromIntegral offset) Nothing
+    parser = sinkParser (fromIntegral `fmap` anyWord16be)
